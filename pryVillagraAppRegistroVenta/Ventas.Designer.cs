279571bb1671a8941008bcc71e7276a25aa656fe
Alumno <Alumno@PC25>
2025-09-01 13:33:11 -0300

    Agregar archivos de proyecto.

---
+++ pryVillagraAppRegistroVenta/Ventas.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace pryVillagraAppRegistroVenta
+{
+    partial class Ventas
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblFecha = new Label();
+            lblProducto = new Label();
+            lblCantidad = new Label();
+            lblPrecioUnitario = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            textBox1 = new TextBox();
+            btnRegistrar = new Button();
+            numericUpDown1 = new NumericUpDown();
+            textBox2 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // lblFecha
+            // 
+            lblFecha.AutoSize = true;
+            lblFecha.Font = new Font("Segoe UI", 12F);
+            lblFecha.Location = new Point(36, 101);
+            lblFecha.Name = "lblFecha";
+            lblFecha.Size = new Size(50, 21);
+            lblFecha.TabIndex = 0;
+            lblFecha.Text = "Fecha";
+            lblFecha.Click += lblFecha_Click;
+            // 
+            // lblProducto
+            // 
+            lblProducto.AutoSize = true;
+            lblProducto.Font = new Font("Segoe UI", 12F);
+            lblProducto.Location = new Point(36, 164);
+            lblProducto.Name = "lblProducto";
+            lblProducto.Size = new Size(73, 21);
+            lblProducto.TabIndex = 1;
+            lblProducto.Text = "Producto";
+            lblProducto.Click += lblProducto_Click;
+            // 
+            // lblCantidad
+            // 
+            lblCantidad.AutoSize = true;
+            lblCantidad.Font = new Font("Segoe UI", 12F);
+            lblCantidad.Location = new Point(37, 244);
+            lblCantidad.Name = "lblCantidad";
+            lblCantidad.Size = new Size(72, 21);
+            lblCantidad.TabIndex = 2;
+            lblCantidad.Text = "Cantidad";
+            // 
+            // lblPrecioUnitario
+            // 
+            lblPrecioUnitario.AutoSize = true;
+            lblPrecioUnitario.Font = new Font("Segoe UI", 12F);
+            lblPrecioUnitario.Location = new Point(36, 309);
+            lblPrecioUnitario.Name = "lblPrecioUnitario";
+            lblPrecioUnitario.Size = new Size(113, 21);
+            lblPrecioUnitario.TabIndex = 3;
+            lblPrecioUnitario.Text = "Precio Unitario";
+            lblPrecioUnitario.Click += label4_Click;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Font = new Font("Segoe UI", 12F);
+            dateTimePicker1.Location = new Point(154, 95);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 29);
+            dateTimePicker1.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 12F);
+            textBox1.Location = new Point(154, 163);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(113, 29);
+            textBox1.TabIndex = 5;
+            // 
+            // btnRegistrar
+            // 
+            btnRegistrar.BackColor = Color.RosyBrown;
+            btnRegistrar.Location = new Point(655, 379);
+            btnRegistrar.Name = "btnRegistrar";
+            btnRegistrar.Size = new Size(119, 41);
+            btnRegistrar.TabIndex = 6;
+            btnRegistrar.Text = "Registrar";
+            btnRegistrar.UseVisualStyleBackColor = false;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Font = new Font("Segoe UI", 12F);
+            numericUpDown1.Location = new Point(154, 231);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(120, 29);
+            numericUpDown1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 12F);
+            textBox2.Location = new Point(155, 301);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(77, 29);
+            textBox2.TabIndex = 8;
+            // 
+            // Ventas
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Silver;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox2);
+            Controls.Add(numericUpDown1);
+            Controls.Add(btnRegistrar);
+            Controls.Add(textBox1);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(lblPrecioUnitario);
+            Controls.Add(lblCantidad);
+            Controls.Add(lblProducto);
+            Controls.Add(lblFecha);
+            Name = "Ventas";
+            Text = "Cantidad";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblFecha;
+        private Label lblProducto;
+        private Label lblCantidad;
+        private Label lblPrecioUnitario;
+        private DateTimePicker dateTimePicker1;
+        private TextBox textBox1;
+        private Button btnRegistrar;
+        private NumericUpDown numericUpDown1;
+        private TextBox textBox2;
+    }
+}
---
+++ pryVillagraAppRegistroVenta/Ventas.cs
@@ -0,0 +1,25 @@
+namespace pryVillagraAppRegistroVenta
+{
+    public partial class Ventas : Form
+    {
+        public Ventas()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblProducto_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblFecha_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
